2078bb9e2d85007240972db65266b41b512bce05
TY0018 <111295767+TY0018@users.noreply.github.com>
2024-09-06 18:12:41 +0800

    Initial Commit

---
+++ dates-log/dates_logApp.swift
@@ -0,0 +1,17 @@
+//
+//  dates_logApp.swift
+//  dates-log
+//
+//  Created by Tong Ying on 6/9/24.
+//
+
+import SwiftUI
+
+@main
+struct dates_logApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
